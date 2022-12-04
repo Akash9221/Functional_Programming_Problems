18ed9d89f5cc678556943afd89cc5b985f570916
“akashmane3938” <maneakash3938@gmail.com>
2022-12-04 04:01:50 -0800

    Functional Programming

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Functional_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FP1_FlipCoin.Flip_Coin();
+            Console.ReadLine();
+
+            FP2_LeapYear.Leap_Year();
+            Console.ReadLine();
+
+            FP3_PowerOf.Power_2();
+            Console.ReadLine();
+
+            FP4_HM.Harmonic_Series();
+            Console.ReadLine();
+
+            FP5_Factor.factors();
+            Console.ReadLine();
+
+            FP6.Quotient();
+            Console.ReadLine();
+
+            FP7_SwapNum.SwappingNumber();
+            Console.ReadLine();
+
+            FP8_EvenOdd.Even_Odd();
+            Console.ReadLine();
+
+            FP9_Vowels.Vowels_Consonent();
+            Console.ReadLine();
+
+            FP10_Latgest3.Latgest_ThreeNumber();
+            Console.ReadLine();
+        }
+    }
+}
