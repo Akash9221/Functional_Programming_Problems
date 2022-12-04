b41d8c13601ef94d0bd27e3ea00da0498cb01f92
“akashmane3938” <maneakash3938@gmail.com>
2022-12-04 04:14:56 -0800

    Check Vowel Or Consonent

---
+++ FP9.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming
+{
+    internal class FP9_Vowels
+    {
+        public static void Vowels_Consonent()
+        {
+            //Accept Characters
+            Console.Write("Enter Character:");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            //Checking Character
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + ":Is Vowel");
+            }
+            else
+            {
+                Console.WriteLine(ch + ":Is Consonant");
+            }
+        }
+    }
+}
