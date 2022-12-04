60fb14fe58b9cdc0510e4d453a626577c69e5ed2
“akashmane3938” <maneakash3938@gmail.com>
2022-12-04 04:12:32 -0800

    Swapping Two Number

---
+++ FP7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming
+{
+    internal class FP7_SwapNum
+    {
+        public static void SwappingNumber()
+        {
+            //Accept Two Value From User
+            Console.WriteLine("Enter The Value For A");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The Value For B");
+            int B = Convert.ToInt32(Console.ReadLine());
+            //Logic For Swap A Two Number
+            A = A * B;
+            B = A / B;
+            A = A / B;
+
+            Console.Write("After swap A= " + A + "B = " + B);
+
+        }
+    }
+}
