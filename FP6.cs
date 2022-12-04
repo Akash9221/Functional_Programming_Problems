fa39b24811512b61045a731dd161d3762373f1a6
“akashmane3938” <maneakash3938@gmail.com>
2022-12-04 04:11:24 -0800

    Find Quotient And Remainder

---
+++ FP6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming
+{
+    internal class FP6
+    {
+        public static void Quotient()
+        {
+            //Accept Divident/Divisior
+            int dividend = 50, divisor = 8;
+
+            //FInd Quotient/Remainder
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
