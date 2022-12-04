ca6eb7ddc64a65cb9bb277a79e4404371dae71fc
“akashmane3938” <maneakash3938@gmail.com>
2022-12-04 04:13:34 -0800

    Find Even Odd Number

---
+++ FP8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming
+{
+    internal class FP8_EvenOdd
+    {
+        public static void Even_Odd()
+        {
+            //Accept Two Values From User
+            Console.WriteLine("Enter The First Value");
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Logic For Check Number Is Even Or Odd
+            if (Num1 % 2 == 0)
+            {
+                Console.WriteLine("The Given Is Even Number");
+            }
+            else
+            {
+                Console.WriteLine("The Given Number Is Odd Number");
+            }
+        }
+    }
+}
