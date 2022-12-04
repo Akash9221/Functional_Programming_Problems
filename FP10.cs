bb0935d9d7439162fb8873475728608807ab6a70
“akashmane3938” <maneakash3938@gmail.com>
2022-12-04 04:16:07 -0800

    Find Largest Among 3 Numbers

---
+++ FP10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming
+{
+    internal class FP10_Latgest3
+    {
+        public static void Latgest_ThreeNumber()
+        {
+            //Accept Three Values From User
+            Console.Write("Input the 1st number :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the  2nd number :");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the 3rd  number :");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)//Check Num1 greater Than Num 
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("The 1st Number is the greatest among three=" + num1);
+                }
+                else
+                {
+                    Console.Write("The 3rd Number is the greatest among three=" + num3);
+                }
+            }
+            else if (num2 > num3)//Check Num2 Greater Than Num3
+                Console.Write("The 2nd Number is the greatest among three =" + num2);
+            else
+                Console.Write("The 3rd Number is the greatest among three =" + num3);
+
+        }
+    }
+}
